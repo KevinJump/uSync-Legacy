4433dc354158738fae73c7e07645335368aaba2d
Kevin Jump <kevin@thejumps.co.uk>
2015-08-28 08:53:09 +0100

    Import Deliverable - for Command Line
    Command Line Importing with files and folders - all seem to work.

--- Jumoo.uSync.Migrations/Deliveriables/uSyncDeliverable.cs
+++ Jumoo.uSync.Migrations/Deliveriables/uSyncDeliverable.cs
@@ -27,7 +27,7 @@ namespace Jumoo.uSync.Migrations.Deliveriables
         {
             var operation = args[0];
 
-            switch(operation.ToLower())
+            switch (operation.ToLower())
             {
                 case "list":
                     break;
@@ -41,7 +41,7 @@ namespace Jumoo.uSync.Migrations.Deliveriables
                 case "import-snapshot":
                     break;
                 case "run-migration":
-                    break;                    
+                    break;
             }
             return DeliverableResponse.Continue;
         }
@@ -60,9 +60,12 @@ namespace Jumoo.uSync.Migrations.Deliveriables
 
             await Out.WriteLineAsync("\n\t uSync will look for files in:");
             await Out.WriteLineAsync("\t\t uSync\\data");
-            await Out.WriteLineAsync("\t\t [more to come]");
+            await Out.WriteLineAsync("\t\t and subfolders of any sepcified folder");
 
             await Out.WriteLineAsync("\n\t\t example : usync import macro/test.config");
+            await Out.WriteLineAsync("\n\t\t\t will search ~/usytnc/data/macro/test.config");
+            await Out.WriteLineAsync("\n\t\t\t\t and any sub folders of ~/uSync/data/macro/");
+
 
         }
 
@@ -76,7 +79,50 @@ namespace Jumoo.uSync.Migrations.Deliveriables
                 return;
             }
 
-            var file = FindFile(args[0]);
+
+            if (args.Length > 1)
+            {
+                switch (args[1].ToLower())
+                {
+                    case "-folder":
+                        // folder import
+                        await ImportFolders(args[0]);
+                        break;
+                }
+            }
+            else
+            {
+                var file = FindFile(args[0]);
+                await ImportFile(file);
+            }
+
+        }
+
+        private async Task ImportFolders(string path)
+        {
+            var fullPath = FindFolder(path);
+            if (String.IsNullOrEmpty(fullPath))
+                return;
+             
+            await Out.WriteLineAsync("Importing Folder: " + fullPath);
+
+            if (Directory.Exists(fullPath))
+            {
+                foreach(var file in Directory.GetFiles(fullPath, "*.config"))
+                {
+                    await ImportFile(file);
+                }
+
+                foreach(var folder in Directory.GetDirectories(fullPath))
+                {
+                    await ImportFolders(folder);
+                }
+            }
+        }
+
+
+        private async Task ImportFile(string file)
+        {
             if (string.IsNullOrEmpty(file))
             {
                 await Out.WriteLineAsync("cannot find the file");
@@ -94,26 +140,160 @@ namespace Jumoo.uSync.Migrations.Deliveriables
             switch (type)
             {
                 case "datatype":
+                    await ImportDataType(node);
                     break;
                 case "documenttype":
+                    await ImportDocType(node);
                     break;
                 case "macro":
-                    var attempt = uSyncCoreContext.Instance.MacroSerializer.DeSerialize(node, true);
-                    if (attempt.Success)
-                    {
-                        await Out.WriteLineAsync(string.Format("Imported {0} ", attempt.Name));
-                    }
-                    else
-                    {
-                        await Out.WriteLineAsync(
-                            string.Format("failed to import {0} : {1}", attempt.Name, attempt.Message));
-                    }
+                    await ImportMacro(node);
+                    break;
+                case "dictionaryitem":
+                    await ImportDictionaryItem(node);
+                    break;
+                case "language":
+                    await ImportLanguage(node);
+                    break;
+                case "mediatype":
+                    await ImportMediaType(node);
+                    break;
+                case "membertype":
+                    await ImportMemberType(node);
                     break;
                 default:
                     await Out.WriteLineAsync("Didn't reconise file type " + type);
                     break;
             }
 
+        }
+
+
+        private async Task ImportDataType(XElement node)
+        {
+            var attempt = uSyncCoreContext.Instance.DataTypeSerializer.DeSerialize(node, true);
+            if (attempt.Success)
+            {
+                await Out.WriteLineAsync("Imported datatype: " + attempt.Name);
+            }
+            else
+            {
+                var error = attempt.Message;
+                if (attempt.Exception != null)
+                    error = error + " " + attempt.Exception.ToString();
+                await Out.WriteLineAsync(string.Format("Failed to import: {0}", error));
+            }
+        }
+
+        private async Task ImportDocType(XElement node)
+        {
+            var attempt = uSyncCoreContext.Instance.ContentTypeSerializer.DeSerialize(node, true);
+            if (attempt.Success)
+            {
+                await Out.WriteLineAsync("Imported datatype: " + attempt.Name);
+            }
+            else
+            {
+                var error = attempt.Message;
+                if (attempt.Exception != null)
+                    error = error + " " + attempt.Exception.ToString();
+                await Out.WriteLineAsync(string.Format("Failed to import: {0}", error));
+            }
+        }
+
+        private async Task ImportMacro(XElement node)
+        {
+            var attempt = uSyncCoreContext.Instance.MacroSerializer.DeSerialize(node, true);
+            if (attempt.Success)
+            {
+                await Out.WriteLineAsync("Imported datatype: " + attempt.Name);
+            }
+            else
+            {
+                var error = attempt.Message;
+                if (attempt.Exception != null)
+                    error = error + " " + attempt.Exception.ToString();
+                await Out.WriteLineAsync(string.Format("Failed to import: {0}", error));
+            }
+        }
+        private async Task ImportDictionaryItem(XElement node)
+        {
+            var attempt = uSyncCoreContext.Instance.DictionarySerializer.DeSerialize(node, true);
+            if (attempt.Success)
+            {
+                await Out.WriteLineAsync("Imported datatype: " + attempt.Name);
+            }
+            else
+            {
+                var error = attempt.Message;
+                if (attempt.Exception != null)
+                    error = error + " " + attempt.Exception.ToString();
+                await Out.WriteLineAsync(string.Format("Failed to import: {0}", error));
+            }
+        }
+        private async Task ImportLanguage(XElement node)
+        {
+            var attempt = uSyncCoreContext.Instance.LanguageSerializer.DeSerialize(node, true);
+            if (attempt.Success)
+            {
+                await Out.WriteLineAsync("Imported datatype: " + attempt.Name);
+            }
+            else
+            {
+                var error = attempt.Message;
+                if (attempt.Exception != null)
+                    error = error + " " + attempt.Exception.ToString();
+                await Out.WriteLineAsync(string.Format("Failed to import: {0}", error));
+            }
+        }
+        private async Task ImportMediaType(XElement node)
+        {
+            var attempt = uSyncCoreContext.Instance.MediaTypeSerializer.DeSerialize(node, true);
+            if (attempt.Success)
+            {
+                await Out.WriteLineAsync("Imported datatype: " + attempt.Name);
+            }
+            else
+            {
+                var error = attempt.Message;
+                if (attempt.Exception != null)
+                    error = error + " " + attempt.Exception.ToString();
+                await Out.WriteLineAsync(string.Format("Failed to import: {0}", error));
+            }
+        }
+        private async Task ImportMemberType(XElement node)
+        {
+            var attempt = uSyncCoreContext.Instance.MemberTypeSerializer.DeSerialize(node, true);
+            if (attempt.Success)
+            {
+                await Out.WriteLineAsync("Imported datatype: " + attempt.Name);
+            }
+            else
+            {
+                var error = attempt.Message;
+                if (attempt.Exception != null)
+                    error = error + " " + attempt.Exception.ToString();
+                await Out.WriteLineAsync(string.Format("Failed to import: {0}", error));
+            }
+        }
+
+        private string FindFolder(string folder)
+        {
+            if (Directory.Exists(folder))
+                return folder;
+
+            var siteRoot = Path.Combine(
+                new FileInfo(Assembly.GetExecutingAssembly().Location).Directory.FullName, "..");
+
+            // try different paths...
+            var path = Path.Combine(siteRoot, "usync", "data", folder);
+
+            if (Directory.Exists(path))
+                return path;
+
+            // do a folder search?
+            // return FindFile(Path.Combine(siteRoot, "usync", "data", Path.GetDirectoryName(file)), Path.GetFileName(file));
+
+            return string.Empty;
         }
 
         private string FindFile(string file)
@@ -130,8 +310,28 @@ namespace Jumoo.uSync.Migrations.Deliveriables
             if (File.Exists(path))
                 return path;
 
+            // do a folder search?
+            // return FindFile(Path.Combine(siteRoot, "usync", "data", Path.GetDirectoryName(file)), Path.GetFileName(file));
+
             return string.Empty;
         }
+
+        private string FindFile(string folder, string name)
+        {
+            var fileName = Path.Combine(folder, name);
+            if (File.Exists(fileName))
+                return fileName;
+
+            foreach (var child in Directory.GetDirectories(folder))
+            {
+                var f = FindFile(child, name);
+                if (!string.IsNullOrEmpty(f))
+                    return f;
+            }
+
+            return string.Empty;
+        }
+
 
     }
 }
