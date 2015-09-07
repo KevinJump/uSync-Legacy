3b9c00cd6fb15393bdcb8bc8bcbe97d6a310f4a6
Kevin Jump <kevin@thejumps.co.uk>
2015-09-07 11:12:22 +0100

    type identification
    Will enable us to be a bit more generic, and good for migration tracking
    when we don't know whats in a file.

---
+++ Jumoo.uSync.Core/Extensions/XElementuSyncTypeExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+
+namespace Jumoo.uSync.Core.Extensions
+{
+    public static class XElementuSyncTypeExtension
+    {
+        /// <summary>
+        ///  returns the type of a XElement based on it's name
+        ///  
+        ///  we use this to work out just what type of thing we
+        ///  have, we can then have a generic serialize/deserialize
+        /// 
+        ///  (but we can also work backwards from unknown xelements)
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public static Type GetUmbracoType(this XElement node)
+        {
+            switch(node.Name.LocalName)
+            {
+                case Constants.Packaging.DocumentTypeNodeName:
+                    return typeof(IContentType);
+                case Constants.Packaging.DictionaryItemNodeName:
+                    return typeof(IDictionaryItem);
+                case Constants.Packaging.LanguagesNodeName:
+                    return typeof(ILanguage);
+                case Constants.Packaging.TemplateNodeName:
+                    return typeof(ITemplate);
+                case Constants.Packaging.MacroNodeName:
+                    return typeof(IMacro);
+                case Constants.Packaging.DataTypeNodeName:
+                    return typeof(IDataTypeDefinition);
+                case "MediaType":
+                    return typeof(IMediaType);
+                default:
+                    return default(Type);
+            }
+        }
+    }
+}
