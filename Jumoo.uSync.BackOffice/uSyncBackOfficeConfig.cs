4e11117d8066a4ce5199b0d658ab2806eaa4bc46
jake williamson <jake.williamson@gibedigital.com>
2016-02-02 16:40:28 +0000

    this update reads in the contents of the config files rather than opening them as a stream. we've also wrapped the saves in a try/catch (not 100% sure if this'll cause usync to fail silently or will display the error in the usync developer tab?).
    the main reason for this is we have environments we upload too that are locked down and we cannot open files using system.io due to permissions. however, in that example we upload existing config files that usync can read in as we don't want the production servers writing to the the configs, we only want them to read.

--- Jumoo.uSync.BackOffice/uSyncBackOfficeConfig.cs
+++ Jumoo.uSync.BackOffice/uSyncBackOfficeConfig.cs
@@ -1,128 +1,132 @@
 ﻿
 namespace Jumoo.uSync.BackOffice
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Xml.Serialization;
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Xml.Serialization;
 
-    using Umbraco.Core.IO;
-    using Umbraco.Core.Logging;
+	using Umbraco.Core.IO;
+	using Umbraco.Core.Logging;
 
-    public class uSyncBackOfficeConfig
-    {
-        private uSyncBackOfficeSettings _settings;
-        public uSyncBackOfficeSettings Settings
-        {
-            get { return _settings; }
-        }
+	public class uSyncBackOfficeConfig
+	{
+		private uSyncBackOfficeSettings _settings;
+		public uSyncBackOfficeSettings Settings
+		{
+			get { return _settings; }
+		}
 
-        public uSyncBackOfficeConfig()
-        {
-            Init();
-        }
+		public uSyncBackOfficeConfig()
+		{
+			Init();
+		}
 
-        private void Init()
-        {
-            if (_settings != null) return;
+		private void Init()
+		{
+			if (_settings != null) return;
 
-            try
-            {
-                var configFile = IOHelper.MapPath(
-                    Path.Combine(SystemDirectories.Config, "uSyncBackOffice.Config"));
+			try
+			{
+				var configFile = IOHelper.MapPath(Path.Combine(SystemDirectories.Config, "uSyncBackOffice.Config"));
 
-                if (System.IO.File.Exists(configFile))
-                {
-                    XmlSerializer serializer = new XmlSerializer(typeof(uSyncBackOfficeSettings));
-                    using (FileStream fs = new FileStream(configFile, FileMode.Open))
-                    {
-                        _settings = (uSyncBackOfficeSettings)serializer.Deserialize(fs);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                LogHelper.Warn<uSyncBackOfficeConfig>("Unable to load the settings: {0}", () => ex);
-            }
+				if (System.IO.File.Exists(configFile))
+				{
+					XmlSerializer serializer = new XmlSerializer(typeof(uSyncBackOfficeSettings));
+					string xml = System.IO.File.ReadAllText(configFile);
+					using (TextReader reader = new StringReader(xml))
+					{
+						_settings = (uSyncBackOfficeSettings)serializer.Deserialize(reader);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				LogHelper.Warn<uSyncBackOfficeConfig>("Unable to load the uSyncBackOffice.Config settings: {0}", () => ex);
+			}
 
-            if (_settings == null)
-            {
-                // default settings...
-                _settings = new uSyncBackOfficeSettings
-                {
-                    Import = true,
-                    ExportAtStartup = false,
-                    ExportOnSave = true,
-                    WatchForFileChanges = false,
-                    ArchiveVersions = false,
+			if (_settings == null)
+			{
+				// default settings...
+				_settings = new uSyncBackOfficeSettings
+				{
+					Import = true,
+					ExportAtStartup = false,
+					ExportOnSave = true,
+					WatchForFileChanges = false,
+					ArchiveVersions = false,
 
-                    Folder = "~/uSync/data/",
-                    ArchiveFolder = "~/uSync/Archive/",
-                    BackupFolder = "~/uSync/Backup/",
+					Folder = "~/uSync/data/",
+					ArchiveFolder = "~/uSync/Archive/",
+					BackupFolder = "~/uSync/Backup/",
 
-                    Handlers = new List<HandlerConfig>()
-                };
+					Handlers = new List<HandlerConfig>()
+				};
 
-                foreach(var handler in uSyncBackOfficeContext.Instance.Handlers)
-                {
-                    _settings.Handlers.Add(new HandlerConfig
-                    {
-                        Name = handler.Name,
-                        Enabled = true
-                    });
-                }
+				foreach (var handler in uSyncBackOfficeContext.Instance.Handlers)
+				{
+					_settings.Handlers.Add(new HandlerConfig
+					{
+						Name = handler.Name,
+						Enabled = true
+					});
+				}
 
-                // save the defaults to disk...
-                SaveSettings(_settings);
-            }
-        }
+				// save the defaults to disk...
+				SaveSettings(_settings);
+			}
+		}
 
-        public void SaveSettings(uSyncBackOfficeSettings settings)
-        {
-            var configFile = IOHelper.MapPath(
-                Path.Combine(SystemDirectories.Config, "uSyncBackOffice.Config"));
+		public void SaveSettings(uSyncBackOfficeSettings settings)
+		{
+			try
+			{
+				var configFile = IOHelper.MapPath(Path.Combine(SystemDirectories.Config, "uSyncBackOffice.Config"));
 
-            if (File.Exists(configFile))
-                File.Delete(configFile);
+				if (File.Exists(configFile)) File.Delete(configFile);
 
-            XmlSerializer serializer = new XmlSerializer(typeof(uSyncBackOfficeSettings));
+				XmlSerializer serializer = new XmlSerializer(typeof(uSyncBackOfficeSettings));
 
-            using (StreamWriter w = new StreamWriter(configFile))
-            {
-                serializer.Serialize(w, settings);
-            }
-        }
-    }
+				using (StreamWriter w = new StreamWriter(configFile))
+				{
+					serializer.Serialize(w, settings);
+				}
+			}
+			catch (Exception ex)
+			{
+				LogHelper.Warn<uSyncBackOfficeConfig>("Unable to save the uSyncBackOffice.Config settings: {0}", () => ex);
+			}
+		}
+	}
 
-    public class HandlerConfig
-    {
-        [XmlAttribute(AttributeName = "Name")]
-        public string Name { get; set; }
+	public class HandlerConfig
+	{
+		[XmlAttribute(AttributeName = "Name")]
+		public string Name { get; set; }
 
-        [XmlAttribute(AttributeName = "Enabled")]
-        public bool Enabled { get; set; }
-    }
+		[XmlAttribute(AttributeName = "Enabled")]
+		public bool Enabled { get; set; }
+	}
 
+	public class uSyncBackOfficeSettings
+	{
+		public string MappedFolder()
+		{
+			return IOHelper.MapPath(Folder);
+		}
 
-    public class uSyncBackOfficeSettings
-    {
-        public string MappedFolder()
-        {
-            return IOHelper.MapPath(Folder);
-        }
+		public bool Import { get; set; }
+		public bool ExportAtStartup { get; set; }
+		public bool ExportOnSave { get; set; }
+		public bool WatchForFileChanges { get; set; }
 
-        public bool Import { get; set; }
-        public bool ExportAtStartup { get; set; }
-        public bool ExportOnSave { get; set; }
-        public bool WatchForFileChanges { get; set; }
+		public bool ArchiveVersions { get; set; }
 
-        public bool ArchiveVersions { get; set; }
+		public string Folder { get; set; }
+		public string ArchiveFolder { get; set; }
+		public string BackupFolder { get; set; }
+		public int MaxArchiveVersionCount { get; set; }
 
-        public string Folder { get; set; }
-        public string ArchiveFolder { get; set; }
-        public string BackupFolder { get; set; }
-        public int MaxArchiveVersionCount { get; set; }
-
-        public List<HandlerConfig> Handlers {get;set;}
-    }
+		public List<HandlerConfig> Handlers { get; set; }
+	}
 }
