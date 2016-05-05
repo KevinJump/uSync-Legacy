b0e0e28b29a76a1edcc44d649c30d00b5773ad75
Kevin Jump <kevin@thejumps.co.uk>
2016-05-05 14:01:57 +0100

    Content Edition Version reporting #48

--- Jumoo.uSync.BackOffice/Controllers/uSyncBackOfficeApiController.cs
+++ Jumoo.uSync.BackOffice/Controllers/uSyncBackOfficeApiController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Umbraco.Core;
+using Umbraco.Core.Logging;
 using Umbraco.Web.Editors;
 using Umbraco.Web.Mvc;
 using Umbraco.Web.WebApi;
@@ -47,10 +49,25 @@ namespace Jumoo.uSync.BackOffice.Controllers
         [HttpGet]
         public BackOfficeSettings GetSettings()
         {
+            string addOnString = "";
+
+            var types = TypeFinder.FindClassesOfType<IuSyncAddOn>();
+            foreach (var t in types)
+            {
+                var typeInstance = Activator.CreateInstance(t) as IuSyncAddOn;
+                if (typeInstance != null)
+                {
+                    LogHelper.Debug<Events>("Loading AddOn Versions: {0}", () => typeInstance.GetVersionInfo());
+                    addOnString = string.Format("{0} [{1}]", addOnString, typeInstance.GetVersionInfo());
+                }
+            }
+
+
             var settings = new BackOfficeSettings()
             {
                 backOfficeVersion = uSyncBackOfficeContext.Instance.Version,
                 coreVersion = uSyncCoreContext.Instance.Version,
+                addOns = addOnString,
                 settings = uSyncBackOfficeContext.Instance.Configuration.Settings,
             };
 
@@ -97,6 +114,12 @@ namespace Jumoo.uSync.BackOffice.Controllers
     {
         public string backOfficeVersion { get; set; }
         public string coreVersion { get; set; }
+        public string addOns { get; set; }
         public uSyncBackOfficeSettings settings { get; set; }
+    }
+
+    public interface IuSyncAddOn
+    {
+        string GetVersionInfo();
     }
 }
---
+++ Jumoo.uSync.Content/ContentEdition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Jumoo.uSync.BackOffice.Controllers;
+
+namespace Jumoo.uSync.Content
+{
+    public class ContentEdition : IuSyncAddOn
+    {
+        public string GetVersionInfo()
+        {
+            return string.Format("uSync.Content: {0}", typeof(Jumoo.uSync.Content.ContentEdition)
+              .Assembly.GetName().Version.ToString());
+        }
+    }
+}
