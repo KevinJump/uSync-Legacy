3d7c1ad3dbe259c14490cc557abd03858cfe9637
Kevin Jump <kevin@thejumps.co.uk>
2018-04-27 12:32:40 +0100

    Updating the dashboard to make things look nicer #177
    First steps in making the dashboard something i don't dread to update.

---
+++ Jumoo.uSync.BackOffice/App_Plugins/uSync/uSyncMainActionController.js
@@ -0,0 +1,66 @@
+﻿(function () {
+
+    'use strict';
+
+    function mainActionController($scope) {
+        var vm = this;
+
+        vm.state = function () {
+            return $scope.reporting ? "busy" : "init";
+        }
+
+        vm.importGroup = {
+            defaultButton: {
+                labelKey: "usync_import",
+                handler: importItems
+            },
+            subButtons: [
+                {
+                    labelKey: "usync_importFull",
+                    handler: importAll
+                },
+                {
+                    labelKey: "usync_report",
+                    handler: report
+                }
+            ]
+        };
+
+        vm.exportGroup = {
+            defaultButton: {
+                labelKey: "usync_export",
+                handler: exportItems
+            },
+            subButtons: [
+                {
+                    labelKey: "usync_exportFull",
+                    handler: exportAll
+                }
+            ]
+        };
+
+        ////////////
+        function importItems() {
+            $scope.import(false);
+        }
+
+        function importAll() {
+            $scope.import(true);
+        }
+
+        function exportItems() {
+            $scope.export(false);
+        }
+
+        function exportAll() {
+            $scope.export(true);
+        }
+
+        function report() {
+            $scope.report();
+        }
+    }
+
+    angular.module('umbraco')
+        .controller('uSync.MainAction.Controller', mainActionController);
+})();
